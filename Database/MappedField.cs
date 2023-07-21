8743abc2d0ca01d9a947716aff755d5dcd917d4a
Valeriyyy <valeriykutsar18@gmail.com>
2023-07-20 21:21:58 -0700

    Created Database project for database related files

---
+++ Database/MappedField.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Database;
+public class MappedField {
+    public int Id { get; set; }
+    [Column("schema_id")]
+    public string SchemaId { get; set; }
+    [Column("salesforce_field_name")]
+    public string SalesforceFieldName { get; set; }
+    [Column("psotgres_field_name")]
+    public string PostgresFieldName { get; set; }
+
+    public override string ToString() {
+        return $"{SchemaId} {SalesforceFieldName} => {PostgresFieldName}";
+    }
+}
